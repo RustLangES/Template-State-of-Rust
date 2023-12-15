bd7796ab600772f93743b0d72faa3147835bc7db
linuxmobile <bdiez19@gmail.com>
2023-12-15 18:54:55 -0300

    Refactor stylesheets and update Astro configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#fc5413',
+				secondary: '#8D4CA8',
+				background: '#331e1f',
+				'background-secondary': '#2a201e',
+				text: '#fff6e6'
+			}
+		},
 	},
 	plugins: [],
 }
